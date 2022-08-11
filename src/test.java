47544f0a86d25a3af05449f5af518c2568094ec2
14harish <harishpc2003@gmail.com>
2022-08-11 15:06:19 +0530

    new

---
+++ src/test.java
@@ -0,0 +1,13 @@
+class student{
+int roll=10;
+String name="harish";
+void print(){
+    System.out.println(name);
+}
+}
+public class test {
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.print();
+    }
+}
