97796f90610dc258a1eed6856a50024b89d939eb
14harish <harishpc2003@gmail.com>
2022-08-17 06:52:47 +0530

    new

--- java Program/Date12/EvenOddpattern.java
+++ java Program/Date12/EvenOddpattern.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class EvenOddpattern {
     public static void main(String[] args) {
-        int arr[]={9, 8, 13, 2, 19, 14,12};
+        int arr[]={9,8,13,2,19,14};
+        //9, 8, 13, 2, 19, 14,12
         Arrays.sort(arr);
         ArrayList<Integer> odd=new ArrayList<>();
         ArrayList<Integer> even=new ArrayList<>();
@@ -17,16 +18,32 @@ public class EvenOddpattern {
                 odd.add(arr[i]);
             }
         }
+        int size=(even.size()>odd.size()) ? odd.size():even.size();
         System.out.println(odd+"-"+even);
+        System.out.println(size);
         if(arr[0]%2==0){
-            for(int i=0;i<even.size();i++){
+            for(int i=0;i<size;i++){
                 full.add(even.get(i));
-                for(int j=0;j<odd.size();j++){
-                    full.add(odd.get(j));
+                full.add(odd.get(i));
                 }
-                }
+            if(even.size()<odd.size()){
+                full.add(odd.get(odd.size()-1));
             }
+            if(even.size()>odd.size()){
+                full.add(even.get(even.size()-1));
+            }
+        }
         else{
+            for(int i=0;i<size;i++){
+                full.add(odd.get(i));
+                full.add(even.get(i));
+                }
+            if(even.size()<odd.size()){
+                full.add(odd.get(odd.size()-1));
+            }
+            if(even.size()>odd.size()){
+                full.add(even.get(even.size()-1));
+            }
         }
          System.out.println(full);
 }
