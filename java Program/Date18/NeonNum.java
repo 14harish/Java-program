d8d6ab5f4121cf324cc014a2f668919e6cdcd3f9
14harish <harishpc2003@gmail.com>
2022-08-18 17:26:21 +0530

    new

---
+++ java Program/Date18/NeonNum.java
@@ -0,0 +1,20 @@
+package Date18;
+
+public class NeonNum {
+    public static void main(String[] args) {
+        int n=9;
+        int e=n*n;
+        int sum=0;
+        while(e!=0){
+            int d=e%10;
+            sum+=d;
+            e=e/10;
+        }
+        if(n==sum){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
