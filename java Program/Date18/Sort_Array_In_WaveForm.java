fcd535caff6de08edc6a2c1bcaca3b15bae7c3d4
14harish <harishpc2003@gmail.com>
2022-08-19 10:47:59 +0530

    new

--- java Program/Date18/Sort_Array_In_WaveForm.java
+++ java Program/Date18/Sort_Array_In_WaveForm.java
@@ -2,7 +2,7 @@ package Date18;
 
 public class Sort_Array_In_WaveForm {
  public static void main(String[] args) {
-    int arr[]={ 24,5,2,67,10,34};
+    int arr[]={ 24,5,2,67,10,34,7};
     for(int i=0;i<arr.length;i++){
       for(int j=i+1;j<arr.length;j++){
          if(arr[i]>arr[j]){
@@ -12,8 +12,23 @@ public class Sort_Array_In_WaveForm {
          }
       }
     }
-    for(int i=0;i<arr.length-1;i++){
-      arr[i]=arr[i+1];
+    int m=0;
+    if(arr.length%2==0){
+      m=0;
+    }
+    else{
+      m=1;
+    }
+    for(int i=0;i<arr.length-m;i++){
+      // try{
+      arr[i]=arr[i]+arr[i+1];
+      arr[i+1]=arr[i]-arr[i+1];
+      arr[i]=arr[i]-arr[i+1];
+      i++;
+      // }
+      // catch(Exception e){
+      
+      // }
     }
     for(int i=0;i<arr.length;i++){
       System.out.println(arr[i]);
