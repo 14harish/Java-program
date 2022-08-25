32f1d7df19caf9ad9c1f7adab72c7a286c71e381
14harish <harishpc2003@gmail.com>
2022-08-25 16:06:53 +0530

    new

--- java Program/Date 25/LexicologyRank.java
+++ java Program/Date 25/LexicologyRank.java
@@ -5,11 +5,15 @@ public class LexicologyRank {
         HashSet<String> h=new HashSet<>();
         while(true){
             h.add(shuffle(s));
-            if(h.size()==6){
+            if(h.size()==fact(s.length)){
                 break;
             }
         }
-        for(String e:h){
+        ArrayList<String> aa=new ArrayList<>(h);
+        Collections.sort(aa);
+        String in=new String(s);
+        aa.indexOf(s);
+        for(String e:aa){
             System.out.println(e);
         }
     }
@@ -20,8 +24,14 @@ public class LexicologyRank {
         char temp=0;
         temp=se[s];
         se[s]=se[e];
-        se[s]=temp;
+        se[e]=temp;
         String str=new String(se);
         return str;
+    }
+    static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return fact(n-1)*n;
     }
 }
