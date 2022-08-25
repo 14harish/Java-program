27e7cb27ea6f7fab79726f1b1114cb6410f80386
14harish <harishpc2003@gmail.com>
2022-08-25 17:57:49 +0530

    new

---
+++ java Program/Date 25/given_values_are_magic_square_or_not.java
@@ -0,0 +1,13 @@
+public class given_values_are_magic_square_or_not {
+    public static void main(String[] args) {
+        int arr[][]={{2,7,6},{9,5,1},{4,3,8}};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            System.out.println(i);
+            sum+=arr[i][0];
+            for(int j=0;j<arr.length;j++){
+            }
+        }
+        System.out.println(sum);
+    }
+}
