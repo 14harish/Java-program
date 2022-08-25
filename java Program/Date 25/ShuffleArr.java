81c708c9494941c21631d2ff547186996ba7f0c3
14harish <harishpc2003@gmail.com>
2022-08-25 12:14:24 +0530

    n

--- java Program/Date 25/ShuffleArr.java
+++ java Program/Date 25/ShuffleArr.java
@@ -2,11 +2,11 @@ import java.util.Random;
 
 public class ShuffleArr {
     public static void main(String[] args) {
-        int arr[]={2,4,5,6,7,8,3,5};
+        int arr[]={2,4,5,6,7,8,3,9};
         int temp=0;
         Random random=new Random();
         int ran=random.nextInt(arr.length);
-        System.out.println(ran);
+       // System.out.println(ran);
         for(int i=0;i<arr.length;i++){
             temp=arr[ran];
             arr[ran]=arr[i];
