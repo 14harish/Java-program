d25d58c1863b0058d7042a65157409e8e17fcc99
14harish <harishpc2003@gmail.com>
2022-08-26 17:10:23 +0530

    26

--- java Program/Date 25/CheckFibooMeth2.java
+++ java Program/Date 25/CheckFibooMeth2.java
@@ -2,9 +2,8 @@ import java.util.*;
 public class CheckFibooMeth2 {
     public static void main(String[] args) {
         int n=5;
-        double sqrt=Math.sqrt(5*n-4);
+        double sqrt=Math.sqrt(5*(n*n)-4);
         String s=sqrt+"";
-        System.out.println(s);
         String arr[]=s.split("[.]");
         System.out.println(arr[1].equals("0"));
     }
