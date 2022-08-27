368076f9e415ef73af4eabc605bbf08f4bdff593
14harish <harishpc2003@gmail.com>
2022-08-27 12:01:38 +0530

    new

--- java Program/Date 26/Consecutive_duplicate.java
+++ java Program/Date 26/Consecutive_duplicate.java
@@ -1,14 +1,13 @@
+import java.util.*;
 public class Consecutive_duplicate {
     public static void main(String[] args) {
-        StringBuffer sc=new StringBuffer("amma");
-        for(int i=0;i<sc.length()-1;i++){
-            if(sc.charAt(i)==sc.charAt(i+1)){
-               sc.replace(i,i+1, " ");
-               sc.replace(i+1,i+2, " ");
+        Scanner sb=new Scanner(System.in);
+        System.out.println("Enter String:");
+        StringBuffer sc=new StringBuffer(" "+sb.nextLine()+" ");
+        for(int i=1;i<sc.length()-1;i++){
+            if(sc.charAt(i-1)!=sc.charAt(i) && sc.charAt(i+1)!=sc.charAt(i)){
+                System.out.print(sc.charAt(i));
             }
         }
-        String s=new String(sc);
-        s=s.replace(" ", "");
-        System.out.println(s);
     }    
 }
