aae0762571159ed8c24893a0834cddb0b2ca9bbf
14harish <harishpc2003@gmail.com>
2022-09-08 15:34:11 +0530

    8.9.22

--- java Program/8.9.22/StringSort.java
+++ java Program/8.9.22/StringSort.java
@@ -1,6 +1,21 @@
 public class StringSort {
     public static void main(String[] args) {
-        String n[]={"Harish","Ajai","Abishake","arun","Deepak"};
-        System.out.println(n[1].compareTo(n[2]));
+        String n[]={"Harish","Ajai","Abishake","Arun","Deepak"};
+        int compare=0;
+        String temp="";
+        for(int i=0;i<n.length;i++){
+            for(int j=i+1;j<n.length;j++){
+                compare=n[i].compareTo(n[j]);
+                if(0<compare){
+                    temp=n[j];
+                    n[j]=n[i];
+                    n[i]=temp;
+                }
+               // System.out.println(compare);
+            }
+        }
+        for(int i=0;i<n.length;i++){
+            System.out.println(n[i]);
+        }
     }
 }
---
+++ java Program/8.9.22/next_char_of_char_in_string.java
@@ -0,0 +1,18 @@
+public class next_char_of_char_in_string {
+    public static void main(String[] args) {
+        String n="computer";
+        StringBuffer sc=new StringBuffer();
+        for(int i=0;i<n.length();i++){
+            if(n.charAt(i)=='z'){
+                sc.append('a');
+            }
+            else if(n.charAt(i)=='Z'){
+                sc.append('A');
+            }
+            else{
+                sc.append((char)(n.charAt(i)+1));
+            }
+        }
+        System.out.println(n+":"+sc);
+    }
+}
