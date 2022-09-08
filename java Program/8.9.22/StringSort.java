1e8a14234e359cf4ddb3cb7f2d718fc6f0129d47
14harish <harishpc2003@gmail.com>
2022-09-08 12:28:25 +0530

    8.9.22

---
+++ java Program/8.9.22/StringSort.java
@@ -0,0 +1,6 @@
+public class StringSort {
+    public static void main(String[] args) {
+        String n[]={"Harish","Ajai","Abishake","arun","Deepak"};
+        System.out.println(n[1].compareTo(n[2]));
+    }
+}
