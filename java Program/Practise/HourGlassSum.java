8cb543f379deddb357230747d17cf8b2d5a8ac2f
14harish <harishpc2003@gmail.com>
2022-09-19 12:25:02 +0530

    kollapam

--- java Program/Practise/HourGlassSum.java
+++ java Program/Practise/HourGlassSum.java
@@ -1,26 +1,26 @@
 public class HourGlassSum {
     public static void main(String[] args) {
         int arr[][]={{1,1,1,0,0,0},{0,1,0,0,0,0},{1,1,1,0,0,0},{0,0,2,4,4,0},{0,0,0,2,0,0},{0,0,1,2,4,0}};
-        // for(int i=0;i<arr.length;i++){
-        //     for(int j=0;j<arr.length;j++){
-        //         System.out.print(arr[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        // int k=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                // System.out.print(arr[i][j]+" "+arr[i][j]);
-                if(i==1 || i==4){
-                   System.out.println(i+" "+i);
-                    // sum+=arr[i][i];
-                  }
-                  else{
-                    //  sum+=arr[i][j];
-                  }
+        for(int i=0;i<arr.length-2;i++){
+            for(int j=0;j<arr.length-2;j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
-
+        System.out.println();
+        int max=0;
+        for(int i=0;i<arr.length-2;i++){
+            for(int j=0;j<arr.length-2;j++){
+             //  System.out.print(arr[i][j]+" ");
+               int sum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+               System.out.println((arr[i][j] +" "+ arr[i][j + 1] +" "+ arr[i][j + 2]) +" "+ (arr[i + 1][j + 1]) +" "+ (arr[i + 2][j] +" "+ arr[i + 2][j + 1] +" "+ arr[i + 2][j + 2]));
+               System.out.println(sum);
+               if(sum>max){
+                max=sum;
+               }
+            }
+            System.out.println();
+        }
+        System.out.println(max);
     }   
 }
