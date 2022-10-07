0d960b47e3ea7d320115e8d25ffb9aa4c9d84de2
14harish <harishpc2003@gmail.com>
2022-10-07 11:27:03 +0530

    new

--- java Program/OOPS/abstractEx.java
+++ java Program/OOPS/abstractEx.java
@@ -1,7 +1,7 @@
 //abstraction is process of hiding the implementation detail for user
 //when declaring abstact class and abstract method that abstract method can be used in other class with different
 //condition by extending the abstract class
-package OOPS;
+//package OOPS;
 abstract class shop{
     abstract int fruitPrice();
 }
