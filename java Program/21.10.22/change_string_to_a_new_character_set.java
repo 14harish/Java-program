4e3023fb9b338ab2b364a865fec279df8bc179a2
14harish <harishpc2003@gmail.com>
2022-10-21 11:05:08 +0530

    new

---
+++ java Program/21.10.22/change_string_to_a_new_character_set.java
@@ -0,0 +1,16 @@
+
+public class change_string_to_a_new_character_set {
+    public static void main(String[] args) {
+        String check="qwertyuiopasdfghjklzxcvbnm";
+        String alpha="abcdefghijklmnopqrstuvwxyz";
+        String input="utta";
+        System.out.println();
+        for(int i=0;i<input.length();i++){
+            for(int j=0;j<check.length();j++){
+                if(input.charAt(i)==check.charAt(j)){  
+                    System.out.println(alpha.charAt(j));                    
+                }
+            }
+    }
+    }    
+}
