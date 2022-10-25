6e0cdca30e7467b332a531764628c8b47d94c7e0
14harish <harishpc2003@gmail.com>
2022-10-25 17:33:17 +0530

    new

---
+++ java Program/OOPS/health_care_professional_to_keep_track_patient_vital_statistics.java
@@ -0,0 +1,21 @@
+class patient{
+    String Name;
+    double weight;
+    double height;
+    patient(String Name,double weight,double height){
+        this.Name=Name;
+        this.weight=weight;
+        this.height=height;
+    }
+    public void main(){
+        System.out.println((weight/(height*height)*703);
+    }
+}
+class patients{
+    
+}
+public class health_care_professional_to_keep_track_patient_vital_statistics {
+    public static void main(String[] args) {
+        
+    }
+}
