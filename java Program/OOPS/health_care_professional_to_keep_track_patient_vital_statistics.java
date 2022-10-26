98d2f3575334c5389e5876b993cb60c0dbe57f95
14harish <harishpc2003@gmail.com>
2022-10-26 14:31:02 +0530

    new

--- java Program/OOPS/health_care_professional_to_keep_track_patient_vital_statistics.java
+++ java Program/OOPS/health_care_professional_to_keep_track_patient_vital_statistics.java
@@ -1,3 +1,5 @@
+import java.util.jar.Attributes.Name;
+
 class patient{
     String Name;
     double weight;
@@ -7,15 +9,44 @@ class patient{
         this.weight=weight;
         this.height=height;
     }
-    public void main(){
-        System.out.println((weight/(height*height)*703);
+    public double IBM(){
+        return (weight/(height*height));
     }
 }
 class patients{
-    
+    patient[] pc=new patient[10];
+    patients(){
+    pc[0]=new patient("Arun",44,1.2);
+    pc[1]=new patient("HarishR",55,1.2);
+    pc[2]=new patient("viswanth",70,1.7);
+    pc[3]=new patient("divakaran uncle",77,1.2);
+    pc[4]=new patient("guru",66,1.2);
+    pc[5]=new patient("deepak",44,1.7);
+    pc[6]=new patient("veera",84,1.5);
+    pc[7]=new patient("bhavan",44,1.0);
+    pc[8]=new patient("hariharan",47,1.2);
+    pc[9]=new patient("barath",54,1.8);
+}
+public void find(){
+    System.out.println("--------------High IBM------------------:");
+    for(int i=0;i<10;i++){
+        //System.out.print(pc[i].Name+":"+pc[i].IBM()+"\n");
+        if(pc[i].IBM()>25){
+            System.out.print(pc[i].Name+":"+pc[i].IBM()+"\n");
+        }
+    }
+    System.out.println("--------------LOW IBM---------------------:");
+    for(int i=0;i<10;i++){
+        //System.out.print(pc[i].Name+":"+pc[i].IBM()+"\n");
+        if(pc[i].IBM()<25){
+            System.out.print(pc[i].Name+":"+pc[i].IBM()+"\n");
+        }
+    }
+}
 }
 public class health_care_professional_to_keep_track_patient_vital_statistics {
     public static void main(String[] args) {
-        
+    patients p=new patients();
+    p.find();
     }
 }
