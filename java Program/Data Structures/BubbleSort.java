5ce2dab49716d81f79570bb7b5034b93e7e8bb31
14harish <harishpc2003@gmail.com>
2022-10-26 15:35:50 +0530

    new

--- java Program/Data Structures/BubbleSort.java
+++ java Program/Data Structures/BubbleSort.java
@@ -2,15 +2,14 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[]={19,2,33,4,56,5};
         int temp=0;
-        for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-1;j++){
                 if(arr[j]>arr[j+1]){
-                    temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                    j=-1;
                 }
             }
-        }
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
