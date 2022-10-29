60ce4cffebe60752fcfe2d8eb29a274b8080df11
14harish <harishpc2003@gmail.com>
2022-10-29 11:31:09 +0530

    new

---
+++ java Program/OOPS/employeeDetail.java
@@ -0,0 +1,23 @@
+class employee{
+    int empno;
+    String empName;
+    char designcode;
+    String dept;
+    int basic;
+    int HRA;
+    int IT;
+    employee(int empno,String empName,char designcode,String dept,int basic,int HRA,int IT){
+    this.empno=empno;
+    this.empName=empName;
+    this.designcode=designcode;
+    this.dept=dept;
+    this.basic=basic;
+    this.HRA=HRA;
+    this.IT=IT;
+    }
+}
+public class employeeDetail {
+    public static void main(String[] args) {
+        
+    }
+}
