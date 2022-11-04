649d0ac1a98c00e89a4808e8f67b8912bb0e1f10
14harish <harishpc2003@gmail.com>
2022-11-04 19:51:03 +0530

    new

--- java Program/test.java
+++ java Program/test.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
 
-    class Test {
+class Test {
         public static void main(String args[]) {
         int x = -4;
         System.out.println(x>>1);
         int y = 4;
         System.out.println(y>>2);
+        char s='a';
+        String sv=String.valueOf(s);
+        System.out.println(sv);
+        int arr[]={10,20,30,40,50};
+        Arrays.toString(arr);
+        String na="harish";
+        String ne="HArish";
+        System.out.println(na.equalsIgnoreCase(ne));
+        // System.out.println(arr);
         }
         }
