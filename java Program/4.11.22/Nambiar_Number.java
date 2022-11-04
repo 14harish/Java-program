aa2b007e17cb8bc7f7b9c3cb038f85348413ac7c
14harish <harishpc2003@gmail.com>
2022-11-04 11:37:18 +0530

    new

---
+++ java Program/4.11.22/Nambiar_Number.java
@@ -0,0 +1,38 @@
+public class Nambiar_Number {
+    public static void main(String[] args) {
+        long n=9880127431l;
+        int sum=0;
+        StringBuffer sc=new StringBuffer();
+        String str=Long.valueOf(n).toString(n);
+        // System.out.println(str);
+        // System.out.println(n);
+        int s=str.charAt(0)-'0';
+        for(int i=0;i<str.length()-1;i++){
+            // System.out.println(s);
+            if(s%2==0){
+                System.out.println(s);
+                sum+=str.charAt(i)-'0';
+                if(sum%2!=0){
+                    sc.append(sum);
+                    sum=str.charAt(i+1)-'0';
+                    s=str.charAt(i+1)-'0';
+                    i++;
+                }
+            }
+            else{
+                sum+=str.charAt(i)-'0';
+                if(sum%2==0){
+                    sc.append(sum);
+                    sum=str.charAt(i+1)-'0';
+                    s=str.charAt(i+1)-'0';
+                    System.out.println(s);
+                    i++;
+                }
+            }
+           
+        }
+        sc.append(str.charAt(str.length()-1));
+        System.out.println(sc);
+
+    }    
+}
