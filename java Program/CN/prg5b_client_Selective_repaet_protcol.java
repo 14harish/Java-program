6b3283ad6a83d26a3ecc793e96e2cbcf5cc97877
14harish <harishpc2003@gmail.com>
2022-11-26 06:26:55 +0530

    new

---
+++ java Program/CN/prg5b_client_Selective_repaet_protcol.java
@@ -0,0 +1,29 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Random;
+
+public class prg5b_client_Selective_repaet_protcol {
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("localhost", 9999);
+        int pos = 0;
+        Random rand = new Random();
+
+        DataInputStream di = new DataInputStream(s.getInputStream());
+        DataOutputStream d = new DataOutputStream(s.getOutputStream());
+
+        int len = di.read();
+        int arr[] = new int[5];
+        pos = rand.nextInt(len);
+        arr[pos] = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != pos) {
+                arr[i] = di.read();
+            }
+        }
+        d.write(pos);
+        arr[pos] = di.read();
+
+        System.out.println("pos Send Successfully");
+    }
+}
---
+++ java Program/CN/prg5b_server_Selective_repaet_protcol.java
@@ -0,0 +1,31 @@
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class prg5b_server_Selective_repaet_protcol {
+    public static void main(String[] args) throws Exception{
+       try{
+        int arr[]={1,2,3,4,5};
+        ServerSocket sc=new ServerSocket(9999);
+        Socket s=sc.accept();
+
+        DataInputStream di=new DataInputStream(s.getInputStream());
+        DataOutputStream d=new DataOutputStream(s.getOutputStream());
+
+        System.out.println("Number packet Sent:"+arr.length);
+        d.write(arr.length);
+        d.flush();
+        for(int i=0;i<arr.length;i++){
+            d.write(arr[i]);
+            d.flush();
+        }
+        int pos=di.read();
+        d.write(pos);
+        } 
+        catch(Exception E){
+            System.out.println(E);
+        }
+    }
+}
