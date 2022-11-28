dbc496f232803e462841c0ba200f1e4eac5ea026
14harish <harishpc2003@gmail.com>
2022-11-28 20:23:28 +0530

    new

---
+++ java Program/CN/leakyBucket.java
@@ -0,0 +1,5 @@
+public class leakyBucket {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java Program/CN/linkState.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class linkState {
+    public static void main(String[] args) {
+        int cost[][]={{0,5,2,3,0},//A
+        {5,0,4,0,3},//B
+        {2,4,0,0,4},//C
+        {3,0,0,0,0},//D
+        {0,3,4,0,0}};//E
+        HashMap<Character,Integer> permanent=new HashMap<>();
+        HashMap<Character,Integer> temp=new HashMap<>();
+
+        for(int i=0;i<cost.length;i++){
+            
+        }
+    }
+}
