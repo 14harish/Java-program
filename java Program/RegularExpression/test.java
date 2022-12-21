a71aecbcb89d2e19df712d8053874cfc4100de56
14harish <harishpc2003@gmail.com>
2022-12-21 16:30:16 +0530

    new

---
+++ java Program/RegularExpression/test.java
@@ -0,0 +1,17 @@
+package RegularExpression;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.*;
+
+public class test {
+    public static void main(String[] args) {
+        // Pattern p=Pattern.compile("[a-z]|[0-9]");//this will match both number and alphabets
+        Pattern p=Pattern.compile("[.]{3}"); //^a starts with a
+        Matcher m=p.matcher("jalsjd9asfl80.g.");
+        System.out.println(m.find());
+       ArrayList<String> s=new ArrayList<>();
+       Collections.sort(null);
+    }
+}
