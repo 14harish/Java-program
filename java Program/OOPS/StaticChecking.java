508085bedf2c3265457a3c0f90e63fff4913b901
14harish <harishpc2003@gmail.com>
2022-12-23 14:25:29 +0530

    new

--- java Program/OOPS/StaticChecking.java
+++ java Program/OOPS/StaticChecking.java
@@ -2,11 +2,16 @@ import java.util.*;
 public class StaticChecking {
             public static void main(String[] args) {       
                 tail d=new tail();
+                tail.regNo=11;
+                d.setdata2(); 
+                d.getdata();
                 d.setdata();
                 d.getdata();
-                System.out.println(tail.a); //static only refer with class name we cannot refer with object 
-                System.out.println(d.b); //instace variable can refer only with object 
+                System.out.println(tail.regNo);//lastly modified will be Stored in static variable
+                System.out.println(tail.a);   //static only refer with class name we cannot refer with object 
+                System.out.println(d.b);     //instace variable can refer only with object 
             }       
+            //Static method is stored in heap memory
     }
     
     class tail{
@@ -30,6 +35,11 @@ public class StaticChecking {
         }
         void setdata(){
             regNo=10;
+            college="ksr";
+            dpet="ece";
+        }
+        void setdata2(){
+            regNo=30;
             college="ksr";
             dpet="cse";
         }
