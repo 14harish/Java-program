627b6136ae908a30a2571b0b0d164895d65b00a7
14harish <harishpc2003@gmail.com>
2023-01-03 09:40:17 +0530

    new

---
+++ java Program/Logic Program/program_to_remove_last_occurrence_of_character_from_the_string.java
@@ -0,0 +1,15 @@
+public class program_to_remove_last_occurrence_of_character_from_the_string {
+    public static void main(String[] args) {
+        String inp="I love programming. I love Codeforwin I basdjksahdkj Iasbfdjk";
+        char a='I';
+        int index=0;
+        StringBuffer sc=new StringBuffer(inp);
+        for(int i=0;i<inp.length();i++){
+            if(inp.charAt(i)==a){
+                index=i;
+            }
+        }
+        sc.replace(index, index+1, "");
+        System.out.println(sc);
+    }
+}
