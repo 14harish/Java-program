1b6eae1a5e368c92c47bf1a7674b402691ae4202
14harish <harishpc2003@gmail.com>
2023-02-14 14:42:35 +0530

    new

--- java Program/Trainig/pattern/SquareNo.java
+++ java Program/Trainig/pattern/SquareNo.java
@@ -3,12 +3,12 @@ package Trainig.pattern;
 public class SquareNo {
     public static void main(String[] args) {
         int n=4;
-        int val=0;
+        int val=n;
         for(int i=0;i<n*2;i++,System.out.println()){
-            for(int j=0;j<n*2;j++){
-                System.out.print(val);
+            for(int j=0;j<n*2-1;j++){
+                System.out.print(val+" ");
             }
-            val++;
+            val--;
         }
     }
     
