9e84aefdafb5488146dd30eff43d424f09273bb8
14harish <harishpc2003@gmail.com>
2023-02-14 13:53:52 +0530

    new

--- java Program/Trainig/pattern/NoPattern.java
+++ java Program/Trainig/pattern/NoPattern.java
@@ -2,6 +2,17 @@ package Trainig.pattern;
 
 public class NoPattern {
     public static void main(String[] args) {
-        
+        int n=5;
+        int val=1;
+        for(int i=0;i<n;i++){
+        for(int k=n;k>i;k--,System.out.println()){
+            for(int j=0;j<=i*2;j++){
+                    System.out.print(val+" ");
+            }
+        }
+            val++;
+        }
+
     }
+
 }
