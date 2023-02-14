0140207f049e966d3d78046dd3b330a22129de06
14harish <harishpc2003@gmail.com>
2023-02-14 11:24:16 +0530

    new

--- java Program/Trainig/pattern/diamond.java
+++ java Program/Trainig/pattern/diamond.java
@@ -1,19 +1,11 @@
-package Trainig;
+package Trainig.pattern;
 
-public class TwoTriangle {
+public class diamond {
     public static void main(String[] args) {
         int n=5;
-
         for(int i=0;i<n*2;i++,System.out.println()){
             int row= i<n ? i :(2*n)-i;
-            for(int j=0;j<row;j++){
-                System.out.print("* ");
-            }
-        }
-        System.out.println();
-        for(int i=0;i<n*2;i++,System.out.println()){
-            int row= i<n ? i :(2*n)-i;
-            for(int s=0;s<(n-row)*2;s++){
+            for(int s=0;s<(n-row);s++){
                 System.out.print(" ");
             }
             for(int j=0;j<row;j++){
--- java Program/Trainig/pattern/leftTriangle.java
+++ java Program/Trainig/pattern/leftTriangle.java
@@ -2,23 +2,23 @@ package Trainig.pattern;
 
 public class leftTriangle {
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         for(int i=0;i<n;i++,System.out.println()){
-            for(int s=1;s<n-i;s++){
+            for(int s=1;s<(n-i)*2;s++){
                 System.out.print(" ");
             }
             for(int j=0;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
         }
 
         System.out.println();
         for(int i=0;i<n;i++,System.out.println()){
-            for(int s=1;s<=((n-i)*2)-2;s++){
+            for(int s=1;s<=((n-i)*4)-4;s++){
                 System.out.print(" ");
             }
             for(int j=0;j<=(i*2);j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
         }
     }
