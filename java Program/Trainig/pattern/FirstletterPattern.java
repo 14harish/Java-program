343d5f53232ab22e88b5f107ff09fb591d5c9cbe
14harish <harishpc2003@gmail.com>
2023-02-14 16:31:55 +0530

    new

---
+++ java Program/Trainig/pattern/FirstletterPattern.java
@@ -0,0 +1,87 @@
+package Trainig.pattern;
+
+public class FirstletterPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if (i == n / 2) {
+                    System.out.print("* ");
+                } else if ((j == 0 || j == n - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if (i == n / 2) {
+                    System.out.print("* ");
+                } else if ((j == 0 || i == 0 || j == n - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if (i == n / 2) {
+                    System.out.print("* ");
+                } else if ((j == 0 || i == 0 || (j == n - 1 && i < n / 2))) {
+                    System.out.print("* ");
+                } else if (i > n / 2) {
+                    if (i == j) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if (j == n/2) {
+                    System.out.print("* ");
+                } else if (i == 0 || i==n-1) {
+                    System.out.print("* ");
+                } 
+                else {
+                    System.out.print("  ");
+                }
+            }
+        }
+
+        System.out.println();
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if ((j == 0 && i<n/2) || i==n/2 || (j==n-1 && i>n/2 )) {
+                    System.out.print("* ");
+                } else if (i == 0 || i==n-1) {
+                    System.out.print("* ");
+                } 
+                else {
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++, System.out.println()) {
+            for (int j = 0; j < n; j++) {
+                if (i == n / 2) {
+                    System.out.print("* ");
+                } else if ((j == 0 || j == n - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+    }
+}
--- java Program/Trainig/pattern/SquareNo.java
+++ java Program/Trainig/pattern/SquareNo.java
@@ -3,13 +3,19 @@ package Trainig.pattern;
 public class SquareNo {
     public static void main(String[] args) {
         int n=4;
-        int val=n;
-        for(int i=0;i<n*2;i++,System.out.println()){
+        int val1=n;
+        int val2=n;
+        int val3=n;
+        int val4=n;
+
+        for(int i=0;i<n*2-1;i++,System.out.println()){
             for(int j=0;j<n*2-1;j++){
-                System.out.print(val+" ");
+                int r=(i<n) ? i : (n*2)-i ;
+                int min=r<j ? r :j;
+                System.out.print(val1-min+" ");
             }
-            val--;
         }
+        
     }
     
 }
