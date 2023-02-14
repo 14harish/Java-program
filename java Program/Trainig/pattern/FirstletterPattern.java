e5dbd4a7f8d783d44aca0efa0114af064966ce7f
14harish <harishpc2003@gmail.com>
2023-02-14 23:00:43 +0530

    new

--- java Program/Trainig/pattern/FirstletterPattern.java
+++ java Program/Trainig/pattern/FirstletterPattern.java
@@ -83,5 +83,26 @@ public class FirstletterPattern {
                 }
             }
         }
+        System.out.println();
+        for(int i=0;i<n;i++,System.out.println()){
+            for(int j=0;j<n;j++){
+                if(j==0 || j==n-1 ||i==0 ||i==n-1){
+                    System.out.print("  ");
+                }else{
+                    System.out.print("* ");
+                }
+            }
+        }
+
+        System.out.println();
+        for(int i=n-1;i>=0;i--,System.out.println()){
+            for(int j=0;j<=i+n;j++){
+                if(j>(n-i)-1 && j!=0){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+        }
     }
 }
---
+++ java Program/Trainig/pattern/heart.java
@@ -0,0 +1,7 @@
+package Trainig.pattern;
+
+public class heart {
+    public static void main(String[] args) {
+        
+    }
+}
