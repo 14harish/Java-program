884eff1b170aa3804baee4dd73f0658762b4bc7e
14harish <harishpc2003@gmail.com>
2023-02-14 22:20:15 +0530

    new

--- java Program/Trainig/pattern/SquareNo.java
+++ java Program/Trainig/pattern/SquareNo.java
@@ -2,17 +2,13 @@ package Trainig.pattern;
 
 public class SquareNo {
     public static void main(String[] args) {
-        int n=4;
+        int n=3;
         int val1=n;
-        int val2=n;
-        int val3=n;
-        int val4=n;
-
         for(int i=0;i<n*2-1;i++,System.out.println()){
             int r=(i<n) ? i : (n*2)-i-2;
             for(int j=0;j<n*2-1;j++){
                 int col=(j<n) ? j : (n*2)-j-2 ; 
-                int min=r<col ? r : col;
+                int min=r<col ? r : col; //finding minimum
                 System.out.print(val1-min+" ");
             }
         }
