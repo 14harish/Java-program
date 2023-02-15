baa70b4045c82e072036f6354347728d1a2df256
14harish <harishpc2003@gmail.com>
2023-02-15 16:22:02 +0530

    new

---
+++ java Program/Trainig/Basic/Factors.java
@@ -0,0 +1,19 @@
+package Trainig.Basic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Factors {
+    public static void main(String[] args) {
+        int n=20;
+        ArrayList<Integer> a1=new ArrayList<>();
+        for(int i=1;i*i<n;i++){ //Math.sqrt alternate i*i<n
+            if(n%i==0){
+                a1.add(i);
+                a1.add(n/i);
+            }
+        }
+        Collections.sort(a1);
+        System.out.println(a1);
+    }
+}
