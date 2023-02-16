b0b1e92883e455a37570e305ce682c3a45c96182
14harish <harishpc2003@gmail.com>
2023-02-16 07:55:55 +0530

    new

--- java Program/Trainig/Basic/FactorInArray.java
+++ java Program/Trainig/Basic/FactorInArray.java
@@ -1,15 +1,25 @@
 package Trainig.Basic;
+import java.util.*;
 
 public class FactorInArray {
     public static void main(String[] args) {
-        int n=10;
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
         long rem[] =new long[10000];
         int size=0;
-        for(int i=0;i*i<n;i++){
+        while(t -->0){
+            size=0;
+            long n=sc.nextLong();
+        for(long i=1;i*i<n;i++){
             if(n%i==0){
-                System.out.println(i+" ");
+                System.out.print(i+" ");
                 rem[size++]=n/i;
             }
         }
+        for(int j=0;j<size;j++){
+            System.out.print(rem[size-1-j]+" ");
+        }
+        System.out.println();
+    }
     }
 }
