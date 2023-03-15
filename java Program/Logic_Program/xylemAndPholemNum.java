a9502d1cc47a8c485e31c5b3f16bd2b87811ad8b
14harish <harishpc2003@gmail.com>
2023-03-15 15:55:17 +0530

    new

---
+++ java Program/Logic_Program/xylemAndPholemNum.java
@@ -0,0 +1,21 @@
+
+public class xylemAndPholemNum {
+    public static void main(String[] args) {
+        int n=17156;
+        char[] arr=(n+"").toCharArray();
+        int sum1=0,sum2=0;
+        for(int i=0;i<arr.length;i++){
+            if(i==0 || i==arr.length-1){
+                sum1+=arr[i]-'0';
+            }else{
+                sum2+=arr[i]-'0';
+            }
+        }
+        System.out.println(sum1+" "+sum2);
+        if(sum1==sum2){
+            System.out.println("Xylem");
+        }else{
+            System.out.println("pholem");
+        }
+    }
+}
